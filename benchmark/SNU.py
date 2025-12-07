c45771cb452a40cdef1377a7c77134fdad34bc4c
Ben Wong <tydfzx@gmail.com>
2025-12-07 12:53:07 -0500

    Feat/Optimize Dino-Rife (#5)
    * feat: add dino dcn and loss weight config
    
    * fix: dino train start epoch
    
    * feat: add SNU-FILM benchmark code
    
    * feat: remove BN and use GELU for FAPM
    
    * feat: add LPIPS for vimeo90k benchmark
    
    ---------
    
    Co-authored-by: Ben <panbenwong@Bens-MacBook-Pro.local>

--- benchmark/SNU.py
+++ benchmark/SNU.py
@@ -1,13 +1,27 @@
 import sys
+import argparse
+from torch.nn import functional as F
 
 sys.path.append(".")
-import cv2
-import math
-import torch
-import numpy as np
+
 from model.pytorch_msssim import ssim_matlab
 from model.RIFE import Model
+import numpy as np
+import torch
+import math
+import cv2
 import lpips
+
+# Select which test file to run
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--testlist",
+    type=str,
+    default="test-easy.txt",
+    help="Path to the SNU-FILM text list (e.g. test-easy.txt)",
+)
+args = parser.parse_args()
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -16,25 +30,34 @@ model.load_model("train_log")
 model.eval()
 model.device()
 
-path = "vimeo_triplet/"
-f = open(path + "tri_testlist.txt", "r")
+path = args.testlist
+f = open(path, "r")
 psnr_list = []
 ssim_list = []
 
 loss_fn_alex = lpips.LPIPS(net="alex").to(device)
 lpips_list = []
 
-for i in f:
-    name = str(i).strip()
-    if len(name) <= 1:
-        continue
-    print(path + "sequences/" + name + "/im1.png")
-    I0 = cv2.imread(path + "sequences/" + name + "/im1.png")
-    I1 = cv2.imread(path + "sequences/" + name + "/im2.png")
-    I2 = cv2.imread(path + "sequences/" + name + "/im3.png")
+
+for i, line in enumerate(f):
+    paths = line.strip().split()
+    print(f"../{paths[0]}")
+    I0 = cv2.imread(f"./{paths[0]}")
+    I1 = cv2.imread(f"./{paths[1]}")
+    I2 = cv2.imread(f"./{paths[2]}")
     I0 = (torch.tensor(I0.transpose(2, 0, 1)).to(device) / 255.0).unsqueeze(0)
     I2 = (torch.tensor(I2.transpose(2, 0, 1)).to(device) / 255.0).unsqueeze(0)
+
+    n, c, h, w = I0.shape
+    ph = ((h - 1) // 32 + 1) * 32
+    pw = ((w - 1) // 32 + 1) * 32
+    padding = (0, pw - w, 0, ph - h)
+    I0 = F.pad(I0, padding)
+    I2 = F.pad(I2, padding)
+
     mid = model.inference(I0, I2)[0]
+    mid = mid[:, :h, :w]
+
     gt_tensor = (
         torch.tensor(I1.transpose(2, 0, 1)).to(device).unsqueeze(0) / 255.0
     )
@@ -45,6 +68,7 @@ for i in f:
     gt_rgb = gt_tensor.flip(1)
     lpips_val = loss_fn_alex(pred_rgb * 2 - 1, gt_rgb * 2 - 1).item()
     lpips_list.append(lpips_val)
+
     mid = (
         np.round((mid * 255).detach().cpu().numpy())
         .astype("uint8")
